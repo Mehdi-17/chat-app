ea1189eb0049974a3d5a40fae01edb71d65d4e3f
Mehdi Serra <mehdi.serra57@gmail.com>
2023-10-29 15:56:30 +0100

    Add features to the chat server : register and unregister client

--- backend/chatServer.go
+++ backend/chatServer.go
@@ -8,4 +8,35 @@ type WsServer struct {
 	unregister chan *Client
 }
 
-//TODO: implement features
+// newWsServer: create a new instance of WsServer
+func newWsServer() *WsServer {
+	return &WsServer{
+		clients:    make(map[*Client]bool),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+// Run : run our webserver and handle requests
+func (server *WsServer) Run() {
+	for {
+		select {
+		case client := <-server.register:
+			server.registerClient(client)
+
+		case client := <-server.unregister:
+			server.unregisterClient(client)
+		}
+	}
+}
+
+// registerClient : register a client to the web server
+func (server *WsServer) registerClient(client *Client) {
+	server.clients[client] = true
+}
+
+func (server *WsServer) unregisterClient(client *Client) {
+	if _, exists := server.clients[client]; exists {
+		delete(server.clients, client)
+	}
+}
